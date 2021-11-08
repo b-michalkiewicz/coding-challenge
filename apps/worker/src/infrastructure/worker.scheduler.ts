44f6ba5b5504cf289b7f6ea119c768cdb553fda5
Bartosz Michalkiewicz <bmichalkiewicz@gmail.com>
2021-11-08 07:51:35 +0100

    Add scheduler logging

--- apps/worker/src/infrastructure/worker.scheduler.ts
+++ apps/worker/src/infrastructure/worker.scheduler.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { CronJob } from 'cron';
 import { WorkerService } from './worker.service';
@@ -8,9 +8,13 @@ export class WorkerScheduler {
     private static readonly cronTime = CronExpression.EVERY_5_MINUTES;
     private static readonly cronName = 'worker-cron';
 
+    private readonly logger = new Logger(WorkerScheduler.name);
+
     constructor(private readonly schedulerRegistry: SchedulerRegistry, private readonly workerService: WorkerService) {}
 
     startDataCollecting() {
+        this.logger.log('Starting new data collection job...');
+
         const job = new CronJob(WorkerScheduler.cronTime, () => this.workerService.runDataPipeline());
         this.schedulerRegistry.addCronJob(WorkerScheduler.cronName, job);
         job.start();
@@ -19,6 +23,8 @@ export class WorkerScheduler {
     }
 
     stopDataCollecting() {
+        this.logger.log('Stopping new data collection job...');
+
         this.schedulerRegistry.deleteCronJob(WorkerScheduler.cronName);
     }
 }
